4a870a8a55b698d598bcefd30f3fac2f81f1d5d6
Fiction_NTV <thevien898@gmail.com>
2022-02-14 19:56:52 +0700

    update login
    update login

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -56,6 +56,56 @@ class LoginScreen extends StatelessWidget {
                 isObscure: true,
               ),
             ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: InkWell(
+                onTap: (){
+                  print("login");
+                },
+                child: const Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Don\'t have an account? ',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                InkWell(
+                  onTap: (){
+                    print("register");
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 20, color: buttonColor),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
