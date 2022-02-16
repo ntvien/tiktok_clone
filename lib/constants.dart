ac614fcad5773b101f02c546e9d513c47bbfdba1
Fiction_NTV <thevien898@gmail.com>
2022-02-16 14:43:44 +0700

    add Bottom Navigation Bar
    add Bottom Navigation Bar

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,14 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok/controllers/auth_controller.dart';
 
+List pages = [
+  Text("Home Screen"),
+  Text("Search Screen"),
+  Text("Add Screen"),
+  Text("Message Screen"),
+  Text("Profile Screen"),
+];
+
 // COLORS
 const backgroundColor = Colors.black;
 const borderColor = Colors.grey;
