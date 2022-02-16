2e690234b2913182b2f5704f1bcd486a9379a41b
Fiction_NTV <thevien898@gmail.com>
2022-02-16 12:45:00 +0700

    firebase setup
    firebase setup

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,42 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+import 'package:tiktok/constants.dart';
 
 class AuthController extends GetxController {
+  // upload to Firebase storage
+  Future<String> _uploadToStorage(File image) async{
+    Reference reference = firebaseStorage
+        .ref()
+        .child("profilePics")
+        .child(firebaseAuth.currentUser!.uid);
 
+    UploadTask uploadTask = reference.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // registering the user
+  Future<void> registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        UserCredential credential =
+            await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+
+      }
+    } catch (e) {
+      Get.snackbar("Error Creating Account", e.toString());
+    }
+  }
 }
